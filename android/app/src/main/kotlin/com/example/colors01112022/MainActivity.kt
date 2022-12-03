8e62fd390ccbd1a6032c09e943d14baa7694ba1e
Pearl Soft <notificationmailtuty@gmail.com>
2022-12-03 12:06:00 +0530

    03122022

---
+++ android/app/src/main/kotlin/com/example/colors01112022/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colors01112022
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() { runApp(MyApp()); }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Flutter Row Example"),
+      ),
+      body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:<Widget>[
+
+            Container(
+              // margin: EdgeInsets.all(12.0),
+              // padding: EdgeInsets.all(8.0),
+              // alignment: Alignment.centerLeft,
+              width: 100,
+              color: Colors.red,
+            ),
+
+
+
+
+            Container(
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children:<Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color:Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color:Colors.green,
+                    ),
+                    ]
+              )
+
+            ),
+
+
+            Container(
+            //   margin: EdgeInsets.all(12.0),
+            //   padding: EdgeInsets.all(8.0),
+            //   decoration:BoxDecoration(
+            //       borderRadius:BorderRadius.circular(8),
+            //       color:Colors.green
+            //   ),
+            //   child: Text("MySQL",style: TextStyle(color:Colors.yellowAccent,fontSize:25),),
+
+      width: 100,
+      color:Colors.blue,
+            )
+          ]
+      ),
+    );
+  }
+}
